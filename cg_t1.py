cf8cab0034f8c1891d3c0ab62f4db45b4108e48f
Thiago Zilberknop <134622097+thiagozilber@users.noreply.github.com>
2025-09-11 16:00:27 -0300

    Enhance entity proximity tracking and visualization

--- cg_t1.py
+++ cg_t1.py
@@ -5,6 +5,7 @@ from math import sqrt, cos, sin
 class World:
     class Entity:
         def __init__(self, id):
+            self.closest = 0.0
             self.id = id
             self.scale = None
             self.frame_info = []
@@ -14,7 +15,7 @@ class World:
             self.y = 0
             self.color = (1.0, 1.0, 1.0)
             self.alpha = 1.0
-            self.radius = 0.2
+            self.radius = 0.05
             
         def update_entity(self, w, h):
             pix_x, pix_y = self.get_next_frame()
@@ -25,13 +26,13 @@ class World:
 
         def draw_self(self):
             gl.glPushMatrix()
-            gl.glTranslatef(self.x, self.y, 0)
+            gl.glTranslatef(self.x, self.y, 0)  # Move the origin to (self.x, self.y)
             gl.glColor4f(*self.color, self.alpha)
             gl.glBegin(gl.GL_TRIANGLE_FAN)
-            gl.glVertex2f(self.x, self.y)  # Center of the circle
+            gl.glVertex2f(0, 0)  # Center of the circle at the new origin
             for i in range(361):  # 360 degrees + 1 to close the circle
                 angle = i * 3.14159 / 180  # Convert degrees to radians
-                gl.glVertex2f(self.x + self.radius * cos(angle), self.y + self.radius * sin(angle))
+                gl.glVertex2f(self.radius * cos(angle), self.radius * sin(angle))  # Circle vertices
             gl.glEnd()
             gl.glPopMatrix()
             
@@ -40,7 +41,7 @@ class World:
                 self.x, self.y, trash = self.frame_info[self.index]
                 self.index += 1
                 return self.x, self.y
-            return -1, -1   # fora da tela
+            return -100, -100   # fora da tela
     
     def __init__(self, filename, w, h):
         self.entities = []
@@ -80,15 +81,20 @@ class World:
     
     def calculate_proximity(self, entity):
         closest = None
+        closest_entity = None
         for other in self.entities:
             if other.id != entity.id:
                 dist = ((entity.x - other.x) ** 2 + (entity.y - other.y) ** 2) ** 0.5
                 if closest is None or dist < closest:
                     closest = dist
+                    closest_entity = other
         if self.player.id != entity.id:
             dist = ((entity.x - self.player.x) ** 2 + (entity.y - self.player.y) ** 2) ** 0.5
             if closest is None or dist < closest:
                 closest = dist
+                closest_entity = self.player
+        entity.closest = closest
+        entity.closest_entity = closest_entity
         return closest
     
     def player_move(self):
@@ -108,12 +114,7 @@ class World:
 
     def handle_proximity(self, entity, dist):
         norm = self.normalize(dist)
-        if entity.id % 2 == 0:
-            entity.color = (1-norm, norm, norm)
-        elif entity.id % 3 == 0:
-            entity.color = (norm, 1-norm, norm)
-        else:
-            entity.color = (norm, norm, 1-norm)
+        entity.color = (norm, norm * 0.5, 1-norm)
         entity.alpha = norm
 
     def update(self):
@@ -128,6 +129,30 @@ class World:
                 entity.index = 0
                 entity.color = (0.0, 0.0, 0.0)
             self.frame = 0
+    
+    def print_distance(self, entity):
+        dist = entity.closest
+        x = entity.x - 0.03
+        y = entity.y + entity.radius + 0.02
+        gl.glColor3f(0.0, 0.0, 0.0)
+        gl.glRasterPos2f(x, y)
+        for char in f"{dist:.2f}":
+            glut.glutBitmapCharacter(glut.GLUT_BITMAP_HELVETICA_12, ord(char))
+
+    def draw_closest_line(self):
+        gl.glColor3f(1.0, 0.0, 0.0)  # Red color for the line
+        gl.glBegin(gl.GL_LINES)
+        # Draw lines for all entities
+        for entity in self.entities:
+            if entity.closest_entity:
+                gl.glVertex2f(entity.x, entity.y)
+                gl.glVertex2f(entity.closest_entity.x, entity.closest_entity.y)
+        # Draw line for the player
+        if self.player.closest_entity:
+            gl.glVertex2f(self.player.x, self.player.y)
+            gl.glVertex2f(self.player.closest_entity.x, self.player.closest_entity.y)
+        gl.glEnd()
+
 
 def main():
     def idle():
@@ -138,7 +163,10 @@ def main():
         gl.glLoadIdentity()
         for entity in world.entities:
             entity.draw_self()
+            world.print_distance(entity)
         world.player.draw_self()
+        world.print_distance(world.player)
+        world.draw_closest_line()  # Draw the line between the closest entities
         glut.glutSwapBuffers()
     def key_down(key, x, y):
         world.keys[key] = True
